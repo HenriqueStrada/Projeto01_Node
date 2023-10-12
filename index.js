d6ef19e9ff24c13ff6832929013011129e5bab7a
Henrique Strada <henriquestradax@gmail.com>
2023-10-12 13:48:41 -0300

    server pronto

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies,
   extended: true
 })); 
 
